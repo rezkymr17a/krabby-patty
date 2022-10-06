6aeb8887ed44c818f1a2466881cba2c127ef094f
rezkymr17a <rezkymr17a@gmail.com>
2022-10-06 10:41:55 +0700

    inisiasi resep krabby patty

---
+++ resep.java
@@ -0,0 +1,11 @@
+public class resep {
+    public static void main(String[] args) {
+    System.out.println("resep krabby patty");
+    System.out.println("------------------");
+    System.out.println("1.bawang");
+    System.out.println("2.ketimun");
+    
+
+    }
+
+}
